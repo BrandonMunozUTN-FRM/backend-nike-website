5cd802a87837a95aff3336fca30ae571428a06ea
Brandon Munoz <byael.munos@gmail.com>
2025-04-20 23:25:05 -0300

    first commit

---
+++ src/main/java/com/backend/nike/backend_nike_website/BackendNikeWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.backend.nike.backend_nike_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendNikeWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendNikeWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/nike/backend_nike_website/BackendNikeWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.nike.backend_nike_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendNikeWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
