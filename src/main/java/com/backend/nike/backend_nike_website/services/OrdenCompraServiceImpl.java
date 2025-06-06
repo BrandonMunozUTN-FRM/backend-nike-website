eb65f5b9e6283a64e5ca0c5455781e2658afb4de
Brandon Munoz <byael.munos@gmail.com>
2025-06-06 09:26:58 -0300

    [FEAT] UPD refactorizacion orden compra

--- src/main/java/com/backend/nike/backend_nike_website/services/OrdenCompraServiceImpl.java
+++ src/main/java/com/backend/nike/backend_nike_website/services/OrdenCompraServiceImpl.java
@@ -1,10 +1,8 @@
 package com.backend.nike.backend_nike_website.services;
 
-import com.backend.nike.backend_nike_website.entities.ItemOrden;
 import com.backend.nike.backend_nike_website.entities.OrdenCompra;
 import com.backend.nike.backend_nike_website.repositories.OrdenCompraRepository;
 import com.backend.nike.backend_nike_website.repositories.ProductoRepository;
-import com.backend.nike.backend_nike_website.repositories.UsuarioRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
@@ -13,37 +11,31 @@ public class OrdenCompraServiceImpl extends BaseServiceImpl<OrdenCompra, Integer
 
     private final OrdenCompraRepository ordenCompraRepository;
     private final ProductoRepository productoRepository;
-    private final UsuarioRepository usuarioRepository;
 
     public OrdenCompraServiceImpl(OrdenCompraRepository ordenCompraRepository,
-            ProductoRepository productoRepository,
-            UsuarioRepository usuarioRepository) {
+                                  ProductoRepository productoRepository) {
         super(ordenCompraRepository);
         this.ordenCompraRepository = ordenCompraRepository;
         this.productoRepository = productoRepository;
-        this.usuarioRepository = usuarioRepository;
     }
 
     @Override
     @Transactional
     public OrdenCompra save(OrdenCompra orden) throws Exception {
-        if (orden.getUsuario() != null && orden.getUsuario().getId() != null) {
-            var usuarioCompleto = usuarioRepository.findById(orden.getUsuario().getId())
-                    .orElseThrow(() -> new Exception("Usuario no encontrado"));
-            orden.setUsuario(usuarioCompleto);
-        }
+        double total = 0.0;
 
-        if (orden.getItems() != null) {
-            for (ItemOrden item : orden.getItems()) {
-                item.setOrdenCompra(orden);
-
-                var productoReal = productoRepository.findById(item.getProducto().getId())
+        if (orden.getProductos() != null) {
+            for (var producto : orden.getProductos()) {
+                var productoReal = productoRepository.findById(producto.getId())
                         .orElseThrow(() -> new Exception("Producto no encontrado"));
-                item.setProducto(productoReal);
+                producto.setNombre(productoReal.getNombre());
+                producto.setPrecio(productoReal.getPrecio());
+                total += productoReal.getPrecio();
             }
         }
 
+        orden.setTotal(total);
+
         return super.save(orden);
     }
-
 }
